168f321fc7d43f892bdfc60dbd4bca3b1cace233
stanislas musoz <2150901@cegepoutaouais.qc.ca>
2026-01-04 11:52:52 -0500

    * Permettre de faire une modification de l'athlètes

--- athletes/script_modifier.js
+++ athletes/script_modifier.js
@@ -41,7 +41,57 @@ async function getAthlete(){
         console.log('Erreur lors du chargement des informations de l\'athlètes: ',error);
     }
 }
+
+async function creerAthletes(event){
+    event.preventDefault();
+    //Chercher les informations du formulaire
+    const id = document.getElementById("id").value;
+    const prenom = document.getElementById("prenom").value;
+    const nom = document.getElementById("nom").value;
+    const alias = document.getElementById("alias").value;
+    const origine = document.getElementById("origine").value;
+    const taille = document.getElementById("taille").value;
+    const poids = document.getElementById("poids").value;
+    const sexe = document.getElementById("sexe").value;
+    const actif = document.getElementsByClassName("actif").value;
+    const date_naissance = document.getElementById("date_naissance").value;
+    const pts_force = parseInt(document.getElementById("force").value);
+    const pts_endurance = parseInt(document.getElementById("endurance").value);
+    const pts_vitesse = parseInt(document.getElementById("vitesse").value);
+    const pts_agilete = parseInt(document.getElementById("agilete").value);
+    const pts_resilience = parseInt(document.getElementById("resilience").value);
+
+    //Créer l'objet Athlete
+    const nouv_Athlete = {id,prenom, nom, alias, origine, taille,poids,
+        sexe, actif, date_naissance, pts_force, pts_endurance,
+        pts_vitesse, pts_agilete, pts_resilience};
+
+    //Faire une requête POST pour envoyer l'objet
+    try{
+        const response = await fetch(`${API_URL}/modifier`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(nouv_Athlete),
+            });
+        if(!response.ok){
+            throw new Error(`Erreur HTTP: ${response.status}`);
+        }
+        console.log("Succèes");
+        window.location.href = "index.html";
+    }
+    catch(error){
+        console.log("Erreur lors de la modification de l'athlète: ",error);
+    }
+}
 //Charger les données losrque c'est prêt
 document.addEventListener("DOMContentLoaded", async () => {
     await getAthlete();
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", creerAthletes);
 });
