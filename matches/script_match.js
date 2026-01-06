03550bc72732779a2bcfa70c0cc3fc89911d2049
stanislas musoz <2150901@cegepoutaouais.qc.ca>
2026-01-06 17:42:18 -0500

    Afficher tous les matches de L'API

---
+++ matches/script_match.js
@@ -0,0 +1,31 @@
+//Lien de l'API
+const API_URL = "http://localhost:8080/api/match";
+
+//Fonction pour charger tous les matches enregistré dans la base de données
+async function chargerMatches() {
+    try {
+        const response = await fetch(`${API_URL}/liste`)
+        if (!response.ok) {
+            throw new Error(`Erreur HTTP: ${response.status}`);
+        }
+
+        const matches = await response.json();
+
+        const container = document.getElementById("affichage");
+        container.innerHTML = "";
+        matches.forEach(matche => {
+            const row = document.createElement("div");
+            row.classList.add("col");
+            row.innerHTML = `
+                <p>${matche.date_evenement}</p>
+                <p>${matche.lieu}</p>`;
+            container.appendChild(row);
+        });
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+//Charger les données lorsque c'est prêt
+document.addEventListener("DOMContentLoaded", chargerMatches);
