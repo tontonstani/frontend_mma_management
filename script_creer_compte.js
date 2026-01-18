d17687e94fcbd134f30031fe5e2c10082f542bbe
stanislas musoz <2150901@cegepoutaouais.qc.ca>
2026-01-18 14:53:56 -0500

    La requête POST pour créer un compte est fonctionnelle

---
+++ script_creer_compte.js
@@ -0,0 +1,38 @@
+//Lien de l'API
+const API_URL = "http://localhost:8080/api/auth";
+
+async function enregistrerUtilisateur(event){
+    event.preventDefault();
+
+    //récupérer les données dans le formulaire
+    const prenom = document.getElementById("prenom").value;
+    const nom = document.getElementById("nom").value;
+    const email = document.getElementById("courriel").value;
+    const password = document.getElementById("password").value;
+
+    //Créer l'objet Utilisateur
+    const user = {prenom,nom,email,password};
+
+    //Envoyer la requête POST vers le serveur
+    try{
+        const response = await fetch(`${API_URL}/register`,{
+            method:"POST",
+            headers:{"content-type":"application/json"},
+            body:JSON.stringify(user)
+        });
+        if(!response.ok){
+            throw new Error(`Erreur HTTP: ${response.status}`);
+        }
+
+        console.log("Succès");
+        window.location.href="connexion.html";
+    }
+    catch(error){
+        console.log("Erreur lors de la création d'un compte: ",error);
+    }
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+    const form = document.querySelector("form");
+    form.addEventListener("submit", enregistrerUtilisateur);
+});
