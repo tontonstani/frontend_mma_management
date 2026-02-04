a09fd2aa8a9f5372ada748ebc84116e261f4502b
stanislas musoz <2150901@cegepoutaouais.qc.ca>
2026-02-04 15:36:53 -0500

    * Corriger les erreurs d'appel du bon formulaire pour exécuter les bonnes actions

--- verification_authentification.js
+++ verification_authentification.js
@@ -1,5 +1,19 @@
+async function deconnexion(event){
+    event.preventDefault();
+
+    //Enlever les token, username et le rôle enregistrés dans le sessionStorage
+    sessionStorage.removeItem("token");
+    sessionStorage.removeItem("role");
+    sessionStorage.removeItem("user");
+
+}
 document.addEventListener("DOMContentLoaded",()=>{
-    const user = sessionStorage.getItem("user");
-    const auth_section = document.getElementById("case_auth_intro");
-    auth_section.innerHTML = `<p>Bonjour <strong>${user}</strong></p>`;
+    if(sessionStorage.getItem("user")!= null){
+        const user = sessionStorage.getItem("user");
+        const auth_section = document.getElementById("case_auth_intro");
+        auth_section.innerHTML = `<p>Bonjour <strong>${user}</strong></p>`;
+    }
+
+    const form = document.querySelector("form");
+    form.addEventListener("submit", deconnexion);
 });
