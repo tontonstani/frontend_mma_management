2a2dc55699253969a64923eb605d31df0852654e
stanislas musoz <2150901@cegepoutaouais.qc.ca>
2026-03-05 21:01:16 -0500

    Ajout: * Afficher la liste des matches en dessous des détails de l'athlète

--- athletes/script_details.js
+++ athletes/script_details.js
@@ -12,17 +12,16 @@ function calculateAge(birthdate) {
     }
     return age;
 }
-console.log(calculateAge('1990-05-15')); // Output: Age as an integer
 
-async function getInfoAthlete(){
+async function getInfoAthlete() {
     //Récupérer le ID
     const params = new URLSearchParams(window.location.search);
     const id = params.get("id");
 
     //Faire la requête GET pour récupérer les informations de l'athlète
-    try{
-        const response = await fetch(`${API_URL}/${id}`);
-        if(!response.ok){
+    try {
+        const response = await fetch(`${API_URL}/DTO/${id}`);
+        if (!response.ok) {
             throw new Error(`Erreur HTTP: ${response.status}`);
         }
         const athlete = await response.json();
@@ -30,13 +29,21 @@ async function getInfoAthlete(){
 
         //Message pour afficher si l'athlète est actif
         var actif = "";
-        if(athlete.actif){
+        if (athlete.actif) {
             actif = "Actif";
-        }
-        else{
+        } else {
             actif = "Retraité";
         }
 
+        //Liste des matches
+        const row = document.createElement("ul");
+        row.classList.add("list-group");
+        athlete.lst_matches.forEach(match => {
+            const element = document.createElement("li");
+            element.classList.add("list-group-item", "text-center");
+            element.textContent = match;
+            row.appendChild(element);
+        })
         //Afficher les informations dans la pages
         const container = document.getElementById('information');
         container.innerHTML = `
@@ -69,80 +76,86 @@ async function getInfoAthlete(){
             </div>
         </div>
 
+        <div class="col-12">
+            <h2 class="text-center">Tous les matches</h2>
+            <div id="matchList"></div>
+        </div>
+
     </div>
 </div>
 `;
+        document.getElementById("matchList").appendChild(row);
 
-                    //Faire afficher un diagramme des données de l'athlète
-                    const ctx = document.getElementById("chart_athlete");
+        //Faire afficher un diagramme des données de l'athlète
+        const ctx = document.getElementById("chart_athlete");
 
-                    new Chart(ctx, {
-                        type:'radar',
-                        data:{
-                            labels:[`Force (${athlete.pts_force})`,
-                                `Endurance (${athlete.pts_endurance})`,
-                                `Vitesse (${athlete.pts_vitesse})`,
-                                `Agilité (${athlete.pts_agilete})`,
-                                `Résilience (${athlete.pts_resilience})`],
-                            datasets:[{
-                                label:`${athlete.prenom} ${athlete.nom}`,
-                                data:[
-                                    athlete.pts_force,
-                                    athlete.pts_endurance,
-                                    athlete.pts_vitesse,
-                                    athlete.pts_agilete,
-                                    athlete.pts_resilience,
-                                ],
-                                fill:true,
-                                backgroundColor:'rgba(153,0,237,0.75)',
-                                borderColor:'rgba(153,0,237,1)',
-                            }]
-                        },
-                        options: {
-                            plugins: {
-                                legend: {
-                                    labels: {
-                                        color:"white",
-                                        font:{
-                                            size: 17,
-                                            weight:'bold'
-                                        }
-                                    }
-                                }
-                            },
-                            elements:{
-                                line:{
-                                    borderWidth:1
-                                }
-                            },
-                            scales:{
-                                r:{
-                                    angleLines:{
-                                        color:'white'
-                                    },
-                                    grid:{
-                                        color:'white',
-                                    },
-                                    suggestedMin:0,
-                                    suggestedMax:10,
-                                    pointLabels:{
-                                        display:'true',
-                                        centerPointLabels:'true',
-                                        color:'white',
-                                        font:{
-                                            size:15
-                                        }
-                                    },
-                                }
+        new Chart(ctx, {
+            type: 'radar',
+            data: {
+                labels: [`Force (${athlete.pts_force})`,
+                    `Endurance (${athlete.pts_endurance})`,
+                    `Vitesse (${athlete.pts_vitesse})`,
+                    `Agilité (${athlete.pts_agilete})`,
+                    `Résilience (${athlete.pts_resilience})`],
+                datasets: [{
+                    label: `${athlete.prenom} ${athlete.nom}`,
+                    data: [
+                        athlete.pts_force,
+                        athlete.pts_endurance,
+                        athlete.pts_vitesse,
+                        athlete.pts_agilete,
+                        athlete.pts_resilience,
+                    ],
+                    fill: true,
+                    backgroundColor: 'rgba(153,0,237,0.75)',
+                    borderColor: 'rgba(153,0,237,1)',
+                }]
+            },
+            options: {
+                plugins: {
+                    legend: {
+                        labels: {
+                            color: "white",
+                            font: {
+                                size: 17,
+                                weight: 'bold'
                             }
                         }
-                    })
+                    }
+                },
+                elements: {
+                    line: {
+                        borderWidth: 1
+                    }
+                },
+                scales: {
+                    r: {
+                        angleLines: {
+                            color: 'white'
+                        },
+                        grid: {
+                            color: 'white',
+                        },
+                        suggestedMin: 0,
+                        suggestedMax: 10,
+                        pointLabels: {
+                            display: 'true',
+                            centerPointLabels: 'true',
+                            color: 'white',
+                            font: {
+                                size: 15
+                            }
+                        },
+                    }
+                }
+            }
+        })
         ;
-    }
-    catch(error){
-        console.log('Erreur lors du chargement des informations de l\'athlètes: ',error)
+    } catch (error) {
+        console.log('Erreur lors du chargement des informations de l\'athlètes: ', error)
     }
 }
+
 //Charger les données losrque c'est prêt
 document.addEventListener("DOMContentLoaded", async () => {
     await getInfoAthlete();
